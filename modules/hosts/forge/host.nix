127c3aca9efd34a76730399f8d81c59091fc4af7
Hutch <will@thehutchery.com>
2026-07-10 10:14:29 -0400

    tmux bind

--- modules/hosts/forge/host.nix
+++ modules/hosts/forge/host.nix
@@ -21,6 +21,7 @@
           services.openssh.enable = true;
 
           dotfiles.tailscale.sshMode = true;
+          dotfiles.tmux.prefix = "C-b";
 
           home-manager.users.hutch = {
             dotfiles.aws.credentialProvider = "default-chain";
--- modules/terminal/tmux.nix
+++ modules/terminal/tmux.nix
@@ -1,67 +1,72 @@
 {
-  flake.modules.homeManager.terminal = {
-    programs.tmux = {
-      enable = true;
-      terminal = "alacritty";
-      historyLimit = 15000;
-      prefix = "C-a";
-      baseIndex = 1;
-      mouse = true;
-      escapeTime = 20;
+  flake.modules.homeManager.terminal =
+    { config, lib, ... }:
+    {
+      options.dotfiles.tmux.prefix = lib.mkOption {
+        type = lib.types.str;
+        default = "C-a";
+        description = "Prefix key used by tmux.";
+      };
 
-      extraConfig = ''
-        set-option -sa terminal-features ',alacritty:RGB'
-        set-option -ga terminal-features ",alacritty:usstyle"
-        set-option -ga terminal-overrides ',alacritty:Tc'
+      config.programs.tmux = {
+        enable = true;
+        terminal = "alacritty";
+        historyLimit = 15000;
+        prefix = config.dotfiles.tmux.prefix;
+        baseIndex = 1;
+        mouse = true;
+        escapeTime = 20;
 
-        set-option -sa terminal-overrides ',screen-256color:Tc'
+        extraConfig = ''
+          set-option -sa terminal-features ',alacritty:RGB'
+          set-option -ga terminal-features ",alacritty:usstyle"
+          set-option -ga terminal-overrides ',alacritty:Tc'
 
-        set -as terminal-overrides ',*:Ss=\E[%p1%d q:Se=\E[2 q'
+          set-option -sa terminal-overrides ',screen-256color:Tc'
 
-        set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
-        set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
+          set -as terminal-overrides ',*:Ss=\E[%p1%d q:Se=\E[2 q'
+          set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
+          set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
 
-        unbind C-b
-        bind-key C-a send-prefix
+          bind-key ${config.dotfiles.tmux.prefix} send-prefix
 
-        bind | split-window -h -c "#{pane_current_path}"
-        bind - split-window -v -c "#{pane_current_path}"
-        unbind '"'
-        unbind %
+          bind | split-window -h -c "#{pane_current_path}"
+          bind - split-window -v -c "#{pane_current_path}"
+          unbind '"'
+          unbind %
 
-        unbind Up
-        unbind Down
-        unbind Left
-        unbind Right
+          unbind Up
+          unbind Down
+          unbind Left
+          unbind Right
 
-        bind r source-file ~/.tmux.conf
+          bind r source-file ~/.tmux.conf
+          bind q kill-session
 
-        bind q kill-session
+          bind-key -r -T prefix Up resize-pane -U
+          bind-key -r -T prefix Down resize-pane -D
+          bind-key -r -T prefix Left resize-pane -L
+          bind-key -r -T prefix Right resize-pane -R
+          bind-key -r -T prefix S-Up resize-pane -U 5
+          bind-key -r -T prefix S-Down resize-pane -D 5
+          bind-key -r -T prefix S-Left resize-pane -L 5
+          bind-key -r -T prefix S-Right resize-pane -R 5
 
-        bind-key -r -T prefix Up resize-pane -U
-        bind-key -r -T prefix Down resize-pane -D
-        bind-key -r -T prefix Left resize-pane -L
-        bind-key -r -T prefix Right resize-pane -R
-        bind-key -r -T prefix S-Up resize-pane -U 5
-        bind-key -r -T prefix S-Down resize-pane -D 5
-        bind-key -r -T prefix S-Left resize-pane -L 5
-        bind-key -r -T prefix S-Right resize-pane -R 5
+          bind -r ^ last-window
+          bind -r k select-pane -U
+          bind -r j select-pane -D
+          bind -r h select-pane -L
+          bind -r l select-pane -R
 
-        bind -r ^ last-window
-        bind -r k select-pane -U
-        bind -r j select-pane -D
-        bind -r h select-pane -L
-        bind -r l select-pane -R
+          set -g set-clipboard on
+          set -g allow-passthrough on
+          set -as terminal-features ',*:clipboard'
+          set -g extended-keys on
+          set -g extended-keys-format csi-u
 
-        set -g set-clipboard on
-        set -g allow-passthrough on
-        set -as terminal-features ',*:clipboard'
-        set -g extended-keys on
-        set -g extended-keys-format csi-u
-
-        bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
-        bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-selection-and-cancel
-      '';
+          bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+          bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-selection-and-cancel
+        '';
+      };
     };
-  };
 }
